84fbfdf3ab9c64caea7306b22f13b011b1c4640b
Semyon Isaev <semacool1@gmail.com>
2021-05-12 00:06:40 +0300

    init project

---
+++ Assets/Scripts/DragTower.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragTower : MonoBehaviour , IDragHandler, IEndDragHandler, IBeginDragHandler
+{
+    [SerializeField] Canvas canvas;
+
+    RectTransform rectTransform;
+    CanvasGroup canvasGroupe;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroupe = GetComponent<CanvasGroup>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroupe.blocksRaycasts = true;
+        canvasGroupe.alpha = 1f;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroupe.blocksRaycasts = false;
+        canvasGroupe.alpha = 0.5f;
+    }
+}
